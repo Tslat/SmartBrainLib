3431fa4f9e5130afdc810a8cd6a6fa8bbebd939d
Tslat <tslatmail@gmail.com>
2022-08-10 10:41:24 +1000

    Create initial setup

---
+++ src/main/java/net/tslat/SmartBrainLib.java
@@ -0,0 +1,18 @@
+package net.tslat;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static net.tslat.SmartBrainLib.MOD_ID;
+
+@Mod(MOD_ID)
+public class SmartBrainLib {
+	public static final String VERSION = "1.0";
+	public static final String MOD_ID = "smartbrainlib";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	public SmartBrainLib() {
+
+	}
+}
