2e18b52f3ba4181837d789e7ecd4b76ad1edd3a0
Tslat <tslatmail@gmail.com>
2022-08-16 17:19:35 +1000

    Swap out example zombie for example skeleton to save time

--- src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLExampleEntities.java
+++ src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLExampleEntities.java
@@ -3,7 +3,7 @@ package net.tslat.smartbrainlib.example.boilerplate;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
@@ -14,13 +14,13 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.tslat.smartbrainlib.SmartBrainLib;
-import net.tslat.smartbrainlib.example.BrainBasedZombie;
+import net.tslat.smartbrainlib.example.SBLSkeleton;
 
 public final class SBLExampleEntities {
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.Keys.ENTITY_TYPES, SmartBrainLib.MOD_ID);
 
 	// Example entities begin ---->
-	public static final RegistryObject<EntityType<BrainBasedZombie>> BRAIN_BASED_ZOMBIE = register("brain_based_zombie", BrainBasedZombie::new, MobCategory.MONSTER, 0.6f, 1.95f);
+	public static final RegistryObject<EntityType<SBLSkeleton>> SBL_SKELETON = register("sbl_skeleton", SBLSkeleton::new, MobCategory.MONSTER, 0.6F, 1.99F);
 	// Example entities end ---->
 
 	private static <T extends LivingEntity> RegistryObject<EntityType<T>> register(String registryName, EntityType.EntityFactory<T> factory, MobCategory category, float width, float height) {
@@ -34,6 +34,6 @@ public final class SBLExampleEntities {
 	}
 
 	private static void registerStats(final EntityAttributeCreationEvent ev) {
-		ev.put(BRAIN_BASED_ZOMBIE.get(), Zombie.createAttributes().build());
+		ev.put(SBL_SKELETON.get(), Skeleton.createAttributes().build());
 	}
 }
--- src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLExampleEntityRenderers.java
+++ src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLExampleEntityRenderers.java
@@ -1,10 +1,10 @@
 package net.tslat.smartbrainlib.example.boilerplate;
 
-import net.minecraft.client.renderer.entity.ZombieRenderer;
+import net.minecraft.client.renderer.entity.SkeletonRenderer;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 
 final class SBLExampleEntityRenderers {
 	static void registerEntityRenderers(final EntityRenderersEvent.RegisterRenderers ev) {
-		ev.registerEntityRenderer(SBLExampleEntities.BRAIN_BASED_ZOMBIE.get(), ZombieRenderer::new);
+		ev.registerEntityRenderer(SBLExampleEntities.SBL_SKELETON.get(), SkeletonRenderer::new);
 	}
 }
