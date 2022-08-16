c13ab03b61a2375634e6fe616664eb1d8a55de89
Tslat <tslatmail@gmail.com>
2022-08-16 17:29:49 +1000

    Add some additional gating behavior wrappers

---
+++ src/main/java/net/tslat/smartbrainlib/core/behaviour/FirstSuccessfulBehaviour.java
@@ -0,0 +1,42 @@
+package net.tslat.smartbrainlib.core.behaviour;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.behavior.Behavior;
+import net.minecraft.world.entity.ai.behavior.GateBehavior;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Group behaviour that attempts to run all sub-behaviours in order, until the first successful one.
+ * @param <E>
+ */
+public final class FirstSuccessfulBehaviour<E extends LivingEntity> extends GateBehavior<E> {
+	public FirstSuccessfulBehaviour(Behavior<? super E>... taskList) {
+		this(ImmutableMap.of(), taskList);
+	}
+
+	public FirstSuccessfulBehaviour(Map<MemoryModuleType<?>, MemoryStatus> memoryRequirements, Behavior<? super E>... taskList) {
+		this(memoryRequirements, behavioursToWeightedList(taskList));
+	}
+
+	public FirstSuccessfulBehaviour(Map<MemoryModuleType<?>, MemoryStatus> memoryRequirements, List<Pair<Behavior<? super E>, Integer>> taskList) {
+		super(memoryRequirements, ImmutableSet.of(), OrderPolicy.ORDERED, RunningPolicy.RUN_ONE, taskList);
+	}
+
+	private static <E extends LivingEntity> List<Pair<Behavior<? super E>, Integer>> behavioursToWeightedList(Behavior<? super E>... tasks) {
+		List<Pair<Behavior<? super E>, Integer>> list = new ObjectArrayList<>();
+
+		for (Behavior<? super E> behaviour : tasks) {
+			list.add(Pair.of(behaviour, 1));
+		}
+
+		return list;
+	}
+}
---
+++ src/main/java/net/tslat/smartbrainlib/core/behaviour/OneRandomBehaviour.java
@@ -0,0 +1,40 @@
+package net.tslat.smartbrainlib.core.behaviour;
+
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.behavior.Behavior;
+import net.minecraft.world.entity.ai.behavior.RunOne;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Wrapper class for {@link RunOne} for easier instantation.
+ * @param <E> The entity
+ */
+public final class OneRandomBehaviour<E extends LivingEntity> extends RunOne<E> {
+	public OneRandomBehaviour(Behavior<? super E>... behaviours) {
+		this(behavioursToWeightedList(behaviours));
+	}
+
+	public OneRandomBehaviour(List<Pair<Behavior<? super E>, Integer>> behaviours) {
+		super(behaviours);
+	}
+
+	public OneRandomBehaviour(Map<MemoryModuleType<?>, MemoryStatus> memoryConditions, List<Pair<Behavior<? super E>, Integer>> behaviours) {
+		super(memoryConditions, behaviours);
+	}
+
+	private static <E extends LivingEntity> List<Pair<Behavior<? super E>, Integer>> behavioursToWeightedList(Behavior<? super E>... tasks) {
+		List<Pair<Behavior<? super E>, Integer>> list = new ObjectArrayList<>();
+
+		for (Behavior<? super E> behaviour : tasks) {
+			list.add(Pair.of(behaviour, 1));
+		}
+
+		return list;
+	}
+}
