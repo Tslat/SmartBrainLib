1314d986902d6a825361ba9f8a06877908177bae
Tslat <tslatmail@gmail.com>
2022-08-18 22:52:02 +1000

    Add registry helper method for serializable memory types

--- src/main/java/net/tslat/smartbrainlib/registry/SBLMemoryTypes.java
+++ src/main/java/net/tslat/smartbrainlib/registry/SBLMemoryTypes.java
@@ -1,5 +1,6 @@
 package net.tslat.smartbrainlib.registry;
 
+import com.mojang.serialization.Codec;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraftforge.registries.DeferredRegister;
@@ -7,6 +8,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.tslat.smartbrainlib.SmartBrainLib;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +22,10 @@ public final class SBLMemoryTypes {
 	public static final RegistryObject<MemoryModuleType<Boolean>> TARGET_UNREACHABLE = register("target_unreachable");
 
 	private static <T> RegistryObject<MemoryModuleType<T>> register(String id) {
-		return MEMORY_TYPES.register(id, () -> new MemoryModuleType<T>(Optional.empty()));
+		return register(id, null);
+	}
+
+	private static <T> RegistryObject<MemoryModuleType<T>> register(String id, @Nullable Codec<T> codec) {
+		return MEMORY_TYPES.register(id, () -> new MemoryModuleType<T>(Optional.ofNullable(codec)));
 	}
 }
