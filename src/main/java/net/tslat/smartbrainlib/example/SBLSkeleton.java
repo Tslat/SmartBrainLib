6c6d25900f49220a61428b7c8dad75454519ff95
Tslat <tslatmail@gmail.com>
2022-08-23 18:26:22 +1000

    Fix SBLSkeleton for new GroupBehaviour changes
    (cherry picked from commit c2a232dc61674f6c75a95b1b75f3e02fe324e3d0)
    
    # Conflicts:
    #	src/main/java/net/tslat/smartbrainlib/example/SBLSkeleton.java

--- src/main/java/net/tslat/smartbrainlib/example/SBLSkeleton.java
+++ src/main/java/net/tslat/smartbrainlib/example/SBLSkeleton.java
@@ -13,23 +13,23 @@ import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BowItem;
 import net.minecraft.world.World;
-import net.minecraft.world.entity.ai.behavior.DoNothing;
 import net.minecraft.world.entity.ai.behavior.LookAtTargetSink;
-import net.minecraft.world.entity.ai.behavior.RandomStroll;
 import net.minecraft.world.entity.ai.behavior.StopAttackingIfTargetInvalid;
 import net.minecraft.world.entity.player.Player;
 import net.tslat.smartbrainlib.api.SmartBrainOwner;
 import net.tslat.smartbrainlib.core.BrainActivityGroup;
 import net.tslat.smartbrainlib.core.SmartBrainProvider;
-import net.tslat.smartbrainlib.core.behaviour.FirstSuccessfulBehaviour;
+import net.tslat.smartbrainlib.core.behaviour.FirstApplicableBehaviour;
 import net.tslat.smartbrainlib.core.behaviour.OneRandomBehaviour;
 import net.tslat.smartbrainlib.core.behaviour.custom.attack.AnimatableMeleeAttack;
 import net.tslat.smartbrainlib.core.behaviour.custom.attack.BowAttack;
 import net.tslat.smartbrainlib.core.behaviour.custom.misc.AvoidSun;
+import net.tslat.smartbrainlib.core.behaviour.custom.misc.Idle;
 import net.tslat.smartbrainlib.core.behaviour.custom.move.AvoidEntity;
 import net.tslat.smartbrainlib.core.behaviour.custom.move.EscapeSun;
 import net.tslat.smartbrainlib.core.behaviour.custom.move.MoveToWalkTarget;
 import net.tslat.smartbrainlib.core.behaviour.custom.move.StrafeTarget;
+import net.tslat.smartbrainlib.core.behaviour.custom.path.SetRandomWalkTarget;
 import net.tslat.smartbrainlib.core.behaviour.custom.target.SetPlayerLookTarget;
 import net.tslat.smartbrainlib.core.behaviour.custom.target.SetRandomLookTarget;
 import net.tslat.smartbrainlib.core.behaviour.custom.target.TargetOrRetaliate;
@@ -45,10 +45,10 @@ public class SBLSkeleton extends SkeletonEntity implements SmartBrainOwner<SBLSk
 		super(entityType, level);
 	}
 
-	// Let's make sure we're definitely not using any goals
+
 	@Override
 	protected final void registerGoals() {}
-
+	// Let's make sure we're definitely not using any goals
 	@Override
 	public final void reassessWeaponGoal() {}
 
@@ -83,20 +83,20 @@ public class SBLSkeleton extends SkeletonEntity implements SmartBrainOwner<SBLSk
 	@Override
 	public BrainActivityGroup<SBLSkeleton> getIdleTasks() {
 		return BrainActivityGroup.idleTasks(
-				new FirstSuccessfulBehaviour<SBLSkeleton>( 				// Run only one of the below behaviours, trying each one in order. Include explicit generic typing because javac is silly
+				new FirstApplicableBehaviour<SBLSkeleton>( 				// Run only one of the below behaviours, trying each one in order. Include explicit generic typing because javac is silly
 						new TargetOrRetaliate<>(),						// Set the attack target
 						new SetPlayerLookTarget<>(),					// Set the look target to a nearby player if available
 						new SetRandomLookTarget<>()), 					// Set the look target to a random nearby location
 				new OneRandomBehaviour<>( 								// Run only one of the below behaviours, picked at random
-						new RandomStroll(1), 				// Set the walk target to a nearby random pathable location
-						new DoNothing(30, 60))); // Don't walk anywhere
+						new SetRandomWalkTarget<>().speedModifier(1), 				// Set the walk target to a nearby random pathable location
+						new Idle<>().runFor(entity -> entity.getRandom().nextInt(30, 60)))); // Don't walk anywhere
 	}
 
 	@Override
 	public BrainActivityGroup<SBLSkeleton> getFightTasks() {
 		return BrainActivityGroup.fightTasks(
 				new StopAttackingIfTargetInvalid<>(target -> !target.isAlive() || target instanceof Player && ((Player)target).isCreative()), 	 // Invalidate the attack target if it's no longer applicable
-				new FirstSuccessfulBehaviour<>( 																							  	 // Run only one of the below behaviours, trying each one in order
+				new FirstApplicableBehaviour<>( 																							  	 // Run only one of the below behaviours, trying each one in order
 						new BowAttack<>(20).startCondition(SBLSkeleton::isHoldingBow),	 												 // Fire a bow, if holding one
 						new AnimatableMeleeAttack<>(0).whenStarting(entity -> setAggressive(true)).whenStarting(entity -> setAggressive(false)))// Melee attack
 		);
