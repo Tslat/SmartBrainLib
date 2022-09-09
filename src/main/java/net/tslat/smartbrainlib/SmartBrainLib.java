8136435bc036d2f3095fb724ffdeb8fe7bdd0430
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-09-09 16:26:27 +0200

    register the goddamn attributes

--- src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java
+++ src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java
@@ -15,7 +15,7 @@ import net.tslat.smartbrainlib.registry.SBLSensors;
 
 @Mod(MOD_ID)
 public class SmartBrainLib {
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 	public static final String MOD_ID = "smartbrainlib";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
@@ -27,7 +27,9 @@ public class SmartBrainLib {
 		SBLSensors.SENSORS.register(modEventBus);
 		SBLMemoryTypes.MEMORY_TYPES.register(modEventBus);
 
-		if (!FMLLoader.isProduction())
+		if (!FMLLoader.isProduction()) {
 			SBLExampleEntities.ENTITY_TYPES.register(modEventBus);
+			SBLExampleEntities.init(modEventBus);
+		}
 	}
 }
