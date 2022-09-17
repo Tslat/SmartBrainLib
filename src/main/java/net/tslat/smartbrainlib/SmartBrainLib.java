588ef2e7bb3cc08bee6ed686b82398ffb5a96378
Tslat <tslatmail@gmail.com>
2022-09-18 06:57:06 +1000

    Update to 1.3

--- src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java
+++ src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java
@@ -14,7 +14,7 @@ import static net.tslat.smartbrainlib.SmartBrainLib.MOD_ID;
 
 @Mod(MOD_ID)
 public class SmartBrainLib {
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.3";
 	public static final String MOD_ID = "smartbrainlib";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
