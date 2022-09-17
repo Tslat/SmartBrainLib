8a96e4afd8aab95ede63f86f8abb8adace95e065
Tslat <tslatmail@gmail.com>
2022-09-18 06:57:06 +1000

    Update to 1.3
    (cherry picked from commit 588ef2e7bb3cc08bee6ed686b82398ffb5a96378)
    
    # Conflicts:
    #	src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java

--- src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java
+++ src/main/java/net/tslat/smartbrainlib/SmartBrainLib.java
@@ -15,7 +15,8 @@ import net.tslat.smartbrainlib.registry.SBLSensors;
 
 @Mod(MOD_ID)
 public class SmartBrainLib {
-	public static final String VERSION = "1.2";
+	
+	public static final String VERSION = "1.3";
 	public static final String MOD_ID = "smartbrainlib";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
