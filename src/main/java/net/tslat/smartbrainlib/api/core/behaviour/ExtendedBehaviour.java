fb8ae75a8a7acbb87212cc3c34792792efa3e01c
Tslat <tslatmail@gmail.com>
2022-11-30 14:28:00 +1000

    Rename stopCondition method
    (cherry picked from commit a91c227d823b0050dbfe9917493e72a587661aca)

--- src/main/java/net/tslat/smartbrainlib/api/core/behaviour/ExtendedBehaviour.java
+++ src/main/java/net/tslat/smartbrainlib/api/core/behaviour/ExtendedBehaviour.java
@@ -112,7 +112,7 @@ public abstract class ExtendedBehaviour<E extends LivingEntity> extends Task<E> 
 	 * @param predicate The predicate
 	 * @return this
 	 */
-	public final ExtendedBehaviour<E> stopCondition(Predicate<E> predicate) {
+	public final ExtendedBehaviour<E> stopIf(Predicate<E> predicate) {
 		this.stopCondition = predicate;
 
 		return this;
