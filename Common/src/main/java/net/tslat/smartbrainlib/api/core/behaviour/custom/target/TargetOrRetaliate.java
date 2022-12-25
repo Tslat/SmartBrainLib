b017c37e03ac17c419c6b554453502e5090fd992
Tslat <tslatmail@gmail.com>
2022-12-25 11:06:13 +1000

    Add alertAllies functionality to the TargetOrRetaliate behaviour

--- Common/src/main/java/net/tslat/smartbrainlib/api/core/behaviour/custom/target/TargetOrRetaliate.java
+++ Common/src/main/java/net/tslat/smartbrainlib/api/core/behaviour/custom/target/TargetOrRetaliate.java
@@ -3,17 +3,22 @@ package net.tslat.smartbrainlib.api.core.behaviour.custom.target;
 import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.ai.memory.NearestVisibleLivingEntities;
 import net.minecraft.world.entity.player.Player;
 import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour;
 import net.tslat.smartbrainlib.util.BrainUtils;
+import net.tslat.smartbrainlib.util.EntityRetrievalUtil;
 
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 /**
@@ -27,6 +32,8 @@ import java.util.function.Predicate;
  * Defaults:
  * <ul>
  *     <li>Targets any live entity, as long as it's not a creative-mode player</li>
+ *     <li>Does not alert nearby allies when retaliating</li>
+ *     <li>If enabled, only alerts allies of the same class, if they don't already have a target themselves</li>
  * </ul>
  * @param <E> The entity
  */
@@ -34,6 +41,8 @@ public class TargetOrRetaliate<E extends Mob> extends ExtendedBehaviour<E> {
 	private static final List<Pair<MemoryModuleType<?>, MemoryStatus>> MEMORY_REQUIREMENTS = ObjectArrayList.of(Pair.of(MemoryModuleType.ATTACK_TARGET, MemoryStatus.VALUE_ABSENT), Pair.of(MemoryModuleType.HURT_BY, MemoryStatus.REGISTERED), Pair.of(MemoryModuleType.NEAREST_ATTACKABLE, MemoryStatus.REGISTERED), Pair.of(MemoryModuleType.NEAREST_VISIBLE_LIVING_ENTITIES, MemoryStatus.REGISTERED));
 
 	protected Predicate<LivingEntity> canAttackPredicate = entity -> entity.isAlive() && (!(entity instanceof Player player) || !player.isCreative());
+	protected BiPredicate<E, Entity> alertAlliesPredicate = (owner, attacker) -> false;
+	protected BiPredicate<E, LivingEntity> allyPredicate = (owner, ally) -> BrainUtils.getTargetOfEntity(ally) != null && owner.getClass().isAssignableFrom(ally.getClass()) && (!(owner instanceof TamableAnimal pet) || pet.getOwner() == ((TamableAnimal)ally).getOwner()) && !ally.isAlliedTo(BrainUtils.getMemory(ally, MemoryModuleType.HURT_BY_ENTITY));
 
 	protected LivingEntity toTarget = null;
 	protected MemoryModuleType<? extends LivingEntity> priorityTargetMemory = MemoryModuleType.NEAREST_ATTACKABLE;
@@ -60,6 +69,29 @@ public class TargetOrRetaliate<E extends Mob> extends ExtendedBehaviour<E> {
 		return this;
 	}
 
+	/**
+	 * Set the predicate to determine whether the brain owner should alert nearby allies of the same entity type when retaliating
+	 * @param predicate The predicate
+	 * @return this
+	 */
+	public TargetOrRetaliate<E> alertAlliesWhen(BiPredicate<E, Entity> predicate) {
+		this.alertAlliesPredicate = predicate;
+
+		return this;
+	}
+
+	/**
+	 * Set the predicate to determine whether a given entity should be alerted to the target as an ally of the brain owner.<br>
+	 * Overriding replaces the default predicate, so be sure to include any portions of the default predicate in your own if applicable
+	 * @param predicate The predicate
+	 * @return this
+	 */
+	public TargetOrRetaliate<E> isAllyIf(BiPredicate<E, LivingEntity> predicate) {
+		this.allyPredicate = predicate;
+
+		return this;
+	}
+
 	@Override
 	protected List<Pair<MemoryModuleType<?>, MemoryStatus>> getMemoryRequirements() {
 		return MEMORY_REQUIREMENTS;
@@ -70,17 +102,26 @@ public class TargetOrRetaliate<E extends Mob> extends ExtendedBehaviour<E> {
 		Brain<?> brain = owner.getBrain();
 		this.toTarget = BrainUtils.getMemory(brain, this.priorityTargetMemory);
 
-		if (this.toTarget == null)
+		if (this.toTarget == null) {
 			this.toTarget = BrainUtils.getMemory(brain, MemoryModuleType.HURT_BY_ENTITY);
 
-		if (this.toTarget == null || !this.canAttackPredicate.test(this.toTarget)) {
-			NearestVisibleLivingEntities nearbyEntities = BrainUtils.getMemory(brain, MemoryModuleType.NEAREST_VISIBLE_LIVING_ENTITIES);
+			if (this.toTarget != null && this.canAttackPredicate.test(this.toTarget)) {
+				if (this.alertAlliesPredicate.test(owner, this.toTarget))
+					alertAllies(level, owner);
 
-			if (nearbyEntities != null)
-				this.toTarget = nearbyEntities.findClosest(this.canAttackPredicate).orElse(null);
+				return true;
+			}
+			else {
+				NearestVisibleLivingEntities nearbyEntities = BrainUtils.getMemory(brain, MemoryModuleType.NEAREST_VISIBLE_LIVING_ENTITIES);
+
+				if (nearbyEntities != null)
+					this.toTarget = nearbyEntities.findClosest(this.canAttackPredicate).orElse(null);
+
+				return this.toTarget != null && this.canAttackPredicate.test(this.toTarget);
+			}
 		}
 
-		return this.toTarget != null && this.canAttackPredicate.test(this.toTarget);
+		return this.canAttackPredicate.test(this.toTarget);
 	}
 
 	@Override
@@ -89,5 +130,14 @@ public class TargetOrRetaliate<E extends Mob> extends ExtendedBehaviour<E> {
 		BrainUtils.clearMemory(entity, MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE);
 
 		this.toTarget = null;
+	}
+
+	protected void alertAllies(ServerLevel level, E owner) {
+		double followRange = owner.getAttributeValue(Attributes.FOLLOW_RANGE);
+
+		for (LivingEntity ally : EntityRetrievalUtil.<LivingEntity>getEntities(level, owner.getBoundingBox().inflate(followRange, 10, followRange),
+				entity -> entity != owner && entity instanceof LivingEntity livingEntity && this.allyPredicate.test(owner, livingEntity))) {
+			BrainUtils.setTargetOfEntity(ally, this.toTarget);
+		}
 	}
 }
