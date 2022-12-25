990d431f421df1ffe2f375dbab8a0cce65599881
Tslat <tslatmail@gmail.com>
2022-12-25 10:34:40 +1000

    Add boilerplate custom behaviour for held actions, akin to CustomBehaviour

---
+++ Common/src/main/java/net/tslat/smartbrainlib/api/core/behaviour/custom/misc/CustomHeldBehaviour.java
@@ -0,0 +1,46 @@
+package net.tslat.smartbrainlib.api.core.behaviour.custom.misc;
+
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
+import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.HeldBehaviour;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * A behaviour module that invokes a callback every tick until stopped.<br>
+ * Useful for handling custom minor actions that are either too specific to warrant a new behaviour, or not worth implementing into a full behaviour.<br>
+ * Set the condition for running via {@link ExtendedBehaviour#startCondition(Predicate)}<br>
+ * Set the condition for stopping via {@link ExtendedBehaviour#stopIf(Predicate)}
+ */
+public final class CustomHeldBehaviour<E extends LivingEntity> extends HeldBehaviour<E> {
+	private Consumer<E> callback;
+
+	public CustomHeldBehaviour(Consumer<E> callback) {
+		this.callback = callback;
+	}
+
+	/**
+	 * Replace the callback function
+	 * @return this
+	 */
+	public CustomHeldBehaviour<E> callback(Consumer<E> callback) {
+		this.callback = callback;
+
+		return this;
+	}
+
+	@Override
+	protected List<Pair<MemoryModuleType<?>, MemoryStatus>> getMemoryRequirements() {
+		return List.of();
+	}
+
+	@Override
+	protected void tick(E entity) {
+		this.callback.accept(entity);
+	}
+}
