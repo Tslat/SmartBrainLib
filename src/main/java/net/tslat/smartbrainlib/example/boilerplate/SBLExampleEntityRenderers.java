b361ebd071c1d1e252363497741112d73c3408d6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-03-25 10:43:44 +0100

    fix SBL not booting in prod

--- src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLExampleEntityRenderers.java
+++ src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLExampleEntityRenderers.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.loading.FMLLoader;
 import net.tslat.smartbrainlib.SmartBrainLib;
 
 @EventBusSubscriber(modid = SmartBrainLib.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -13,6 +14,8 @@ final class SBLExampleEntityRenderers {
 	
 	@SubscribeEvent
 	public static void onClientSetup(final FMLClientSetupEvent event) {
-		RenderingRegistry.registerEntityRenderingHandler(SBLExampleEntities.SBL_SKELETON.get(), SkeletonRenderer::new);
+		if (!FMLLoader.isProduction()) {
+			RenderingRegistry.registerEntityRenderingHandler(SBLExampleEntities.SBL_SKELETON.get(), SkeletonRenderer::new);
+		}
 	}
 }
