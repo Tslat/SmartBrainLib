f5a2fb80bc5e323ae445f1b48740195b12c5a95e
Tslat <tslatmail@gmail.com>
2023-09-08 14:23:55 +1000

    Update to 1.12

--- Common/src/main/java/net/tslat/smartbrainlib/SBLConstants.java
+++ Common/src/main/java/net/tslat/smartbrainlib/SBLConstants.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.ServiceLoader;
 
 public class SBLConstants {
-	public static final String VERSION = "1.11.2";
+	public static final String VERSION = "1.12";
 	public static final String MOD_ID = "smartbrainlib";
 	public static final String MOD_NAME = "SmartBrainLib";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
