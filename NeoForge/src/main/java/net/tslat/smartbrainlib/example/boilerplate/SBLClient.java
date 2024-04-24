1a8079e5d402e3e66d5b8d7acd551f3b91c80dee
Tslat <tslatmail@gmail.com>
2024-04-24 15:49:14 +1000

    Update NeoForge to 1.20.5, convert to darkhax curseforgegradle

--- NeoForge/src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLClient.java
+++ NeoForge/src/main/java/net/tslat/smartbrainlib/example/boilerplate/SBLClient.java
@@ -3,12 +3,12 @@ package net.tslat.smartbrainlib.example.boilerplate;
 import net.minecraft.client.renderer.entity.SkeletonRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.tslat.smartbrainlib.SBLConstants;
 import net.tslat.smartbrainlib.SBLNeoForge;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 final class SBLClient {
 	@SubscribeEvent
 	public static void registerEntityRenderers(final EntityRenderersEvent.RegisterRenderers ev) {
