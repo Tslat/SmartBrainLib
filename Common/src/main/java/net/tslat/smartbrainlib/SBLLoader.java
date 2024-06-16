8bb72c4dc1a4869c829163820bf99f03b27eca0e
Tslat <tslatmail@gmail.com>
2024-06-16 10:09:43 +1000

    Mark registry methods as internal

--- Common/src/main/java/net/tslat/smartbrainlib/SBLLoader.java
+++ Common/src/main/java/net/tslat/smartbrainlib/SBLLoader.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -12,8 +13,10 @@ public interface SBLLoader {
 	void init(Object eventBus);
 	boolean isDevEnv();
 
+	@ApiStatus.Internal
 	<T> Supplier<MemoryModuleType<T>> registerMemoryType(String id);
+	@ApiStatus.Internal
 	<T> Supplier<MemoryModuleType<T>> registerMemoryType(String id, Optional<Codec<T>> codec);
-
+	@ApiStatus.Internal
 	<T extends ExtendedSensor<?>> Supplier<SensorType<T>> registerSensorType(String id, Supplier<T> sensor);
 }
