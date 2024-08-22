2ae913217500969727abf491dc16ea4be532f797
Tslat <tslatmail@gmail.com>
2024-08-23 09:08:39 +1000

    Update SBLSkeleton example and cleanup

--- Common/src/main/java/net/tslat/smartbrainlib/example/SBLSkeleton.java
+++ Common/src/main/java/net/tslat/smartbrainlib/example/SBLSkeleton.java
@@ -26,6 +26,7 @@ import net.tslat.smartbrainlib.api.core.behaviour.custom.move.EscapeSun;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.move.StrafeTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetPlayerLookTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
@@ -55,48 +56,64 @@ public class SBLSkeleton extends Skeleton implements SmartBrainOwner<SBLSkeleton
 		return new SmartBrainProvider<>(this);
 	}
 
+	// Add our sensors - these handle passively detecting and remembering nearby environmental factors
 	@Override
 	public List<? extends ExtendedSensor<? extends SBLSkeleton>> getSensors() {
 		return ObjectArrayList.of(
-				new NearbyPlayersSensor<>(), 							// Keep track of nearby players
-				new NearbyLivingEntitySensor<SBLSkeleton>()
+				new NearbyPlayersSensor<>(), // Keep track of nearby players
+				new NearbyLivingEntitySensor<SBLSkeleton>() // Keep track of nearby entities the Skeleton is interested in
 						.setPredicate((target, entity) ->
 								target instanceof Player ||
 								target instanceof IronGolem ||
 								target instanceof Wolf ||
 								(target instanceof Turtle turtle && turtle.isBaby() && !turtle.isInWater())));
-	}																	// Keep track of nearby entities the Skeleton is interested in
+	}
 
+	// Add our core tasks - this group runs every tick regardless of any other activities the skeleton may be running
 	@Override
 	public BrainActivityGroup<? extends SBLSkeleton> getCoreTasks() {
 		return BrainActivityGroup.coreTasks(
-				new AvoidSun<>(),																							// Keep pathfinder avoiding the sun
-				new EscapeSun<>().cooldownFor(entity -> 20),													// Escape the sun
-				new AvoidEntity<>().avoiding(entity -> entity instanceof Wolf),												// Run away from wolves
-				new LookAtTarget<>().runFor(entity -> entity.getRandom().nextIntBetweenInclusive(40, 300)), 														// Look at the look target
-				new StrafeTarget<>().stopStrafingWhen(entity -> !isHoldingBow(entity)).startCondition(SBLSkeleton::isHoldingBow),	// Strafe around target
-				new MoveToWalkTarget<>());																					// Move to the current walk target
+				new AvoidSun<>(), // Keep pathfinder avoiding the sun
+				new EscapeSun<>() // Escape the sun
+						.cooldownFor(entity -> 20),
+				new AvoidEntity<>() // Run away from wolves
+						.avoiding(entity -> entity instanceof Wolf),
+				new LookAtTarget<>() // Look at the look target
+						.runFor(entity -> entity.getRandom().nextIntBetweenInclusive(40, 300)),
+				new StrafeTarget<>()	// Strafe around target
+						.stopStrafingWhen(entity -> !isHoldingBow(entity))
+						.startCondition(SBLSkeleton::isHoldingBow),
+				new MoveToWalkTarget<>()); // Move to the current walk target
 	}
 
+	// Add our idle tasks - this group runs automatically if no other activites are running (such as fighting)
 	@Override
 	public BrainActivityGroup<? extends SBLSkeleton> getIdleTasks() {
 		return BrainActivityGroup.idleTasks(
-				new FirstApplicableBehaviour<SBLSkeleton>( 				// Run only one of the below behaviours, trying each one in order. Include explicit generic typing because javac is silly
-						new TargetOrRetaliate<>(),						// Set the attack target
-						new SetPlayerLookTarget<>(),					// Set the look target to a nearby player if available
-						new SetRandomLookTarget<>()), 					// Set the look target to a random nearby location
-				new OneRandomBehaviour<>( 								// Run only one of the below behaviours, picked at random
-						new SetRandomWalkTarget<>().speedModifier(1), 				// Set the walk target to a nearby random pathable location
-						new Idle<>().runFor(entity -> entity.getRandom().nextInt(30, 60)))); // Don't walk anywhere
+				new FirstApplicableBehaviour<SBLSkeleton>( // Run only one of the below behaviours, trying each one in order. Include explicit generic typing because javac is silly
+						new TargetOrRetaliate<>(), // Set the attack target
+						new SetPlayerLookTarget<>(), // Set the look target to a nearby player if available
+						new SetRandomLookTarget<>()), // Set the look target to a random nearby location
+				new OneRandomBehaviour<>( // Run only one of the below behaviours, picked at random
+						new SetRandomWalkTarget<>() // Set the walk target to a nearby random pathable location
+								.speedModifier(1),
+						new Idle<>() // Don't do anything for a bit
+								.runFor(entity -> entity.getRandom().nextInt(30, 60))));
 	}
 
+	// Add our fight tasks - this group only runs when the skeleton has a target to attack, as dictated by TargetOrRetaliate
 	@Override
 	public BrainActivityGroup<? extends SBLSkeleton> getFightTasks() {
 		return BrainActivityGroup.fightTasks(
-				new InvalidateAttackTarget<>(), 	 // Invalidate the attack target if it's no longer applicable
-				new FirstApplicableBehaviour<>( 																							  	 // Run only one of the below behaviours, trying each one in order
-						new BowAttack<>(20).startCondition(SBLSkeleton::isHoldingBow),	 												 // Fire a bow, if holding one
-						new AnimatableMeleeAttack<>(0).whenStarting(entity -> setAggressive(true)).whenStarting(entity -> setAggressive(false)))// Melee attack
+				new InvalidateAttackTarget<>(), // Invalidate the attack target if it's no longer applicable
+				new SetWalkTargetToAttackTarget<>() // Run at the target if not holding a bow
+						.startCondition(entity -> !isHoldingBow(entity) && (!entity.level().isDay() || (entity.isOnFire() && entity.level().canSeeSky(entity.blockPosition())))),
+				new FirstApplicableBehaviour<>( // Run only one of the below behaviours, trying each one in order
+						new BowAttack<>(20) // Fire a bow, if holding one
+								.startCondition(SBLSkeleton::isHoldingBow),
+						new AnimatableMeleeAttack<>(0) // Melee attack
+								.whenStarting(entity -> setAggressive(true))
+								.whenStopping(entity -> setAggressive(false)))
 		);
 	}
 
