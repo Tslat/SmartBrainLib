b01cbd8db04a6b75dd8233150d10c7e59495b061
Tslat <tslatmail@gmail.com>
2024-08-23 09:57:12 +1000

    Move to using the extended sensory methods

--- Common/src/main/java/net/tslat/smartbrainlib/api/core/sensor/vanilla/NearbyPlayersSensor.java
+++ Common/src/main/java/net/tslat/smartbrainlib/api/core/sensor/vanilla/NearbyPlayersSensor.java
@@ -13,6 +13,7 @@ import net.tslat.smartbrainlib.object.SquareRadius;
 import net.tslat.smartbrainlib.registry.SBLSensors;
 import net.tslat.smartbrainlib.util.BrainUtils;
 import net.tslat.smartbrainlib.util.EntityRetrievalUtil;
+import net.tslat.smartbrainlib.util.SensoryUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Comparator;
@@ -94,7 +95,7 @@ public class NearbyPlayersSensor<E extends LivingEntity> extends PredicateSensor
 
 		List<Player> attackablePlayers = new ObjectArrayList<>(targetablePlayers);
 
-		attackablePlayers.removeIf(pl -> !isEntityAttackable(entity, pl));
+		attackablePlayers.removeIf(pl -> !SensoryUtils.isEntityAttackable(entity, pl));
 
 		BrainUtils.setMemory(entity, MemoryModuleType.NEAREST_PLAYERS, players);
 		BrainUtils.setMemory(entity, MemoryModuleType.NEAREST_VISIBLE_PLAYER, targetablePlayers.isEmpty() ? null : targetablePlayers.get(0));
