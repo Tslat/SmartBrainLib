7dfe77b400ce5da0038cfe9c15ed9f33551101c8
Tslat <tslatmail@gmail.com>
2024-08-31 16:21:49 +1000

    Auto-expire entity tracker look targets to prevent entities holding onto dead targets

--- Common/src/main/java/net/tslat/smartbrainlib/api/core/behaviour/custom/look/LookAtTarget.java
+++ Common/src/main/java/net/tslat/smartbrainlib/api/core/behaviour/custom/look/LookAtTarget.java
@@ -2,7 +2,10 @@ package net.tslat.smartbrainlib.api.core.behaviour.custom.look;
 
 import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.behavior.EntityTracker;
+import net.minecraft.world.entity.ai.behavior.PositionTracker;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour;
@@ -12,6 +15,9 @@ import java.util.List;
 
 /**
  * Look at the look target for as long as it is present
+ * <p>
+ * Additionally, invalidates the look target if it is an {@link EntityTracker} and the entity has expired
+ *
  * @param <E> The entity
  */
 public class LookAtTarget<E extends Mob> extends ExtendedBehaviour<E> {
@@ -27,12 +33,37 @@ public class LookAtTarget<E extends Mob> extends ExtendedBehaviour<E> {
 	}
 
 	@Override
+	protected boolean checkExtraStartConditions(ServerLevel level, E entity) {
+		return testAndInvalidateLookTarget(entity);
+	}
+
+	@Override
 	protected boolean shouldKeepRunning(E entity) {
-		return BrainUtils.hasMemory(entity, MemoryModuleType.LOOK_TARGET);
+		return testAndInvalidateLookTarget(entity);
 	}
 
 	@Override
 	protected void tick(E entity) {
 		BrainUtils.withMemory(entity, MemoryModuleType.LOOK_TARGET, target -> entity.getLookControl().setLookAt(target.currentPosition()));
+	}
+
+	/**
+	 * Check and expire the look target if it is no longer valid
+	 *
+	 * @return true if the look target is valid
+	 */
+	protected boolean testAndInvalidateLookTarget(E entity) {
+		PositionTracker lookTarget = BrainUtils.getMemory(entity, MemoryModuleType.LOOK_TARGET);
+
+		if (lookTarget == null)
+			return false;
+
+		if (lookTarget instanceof EntityTracker entityTracker && !entityTracker.getEntity().isAlive()) {
+			BrainUtils.clearMemory(entity, MemoryModuleType.LOOK_TARGET);
+
+			return false;
+		}
+
+		return true;
 	}
 }
